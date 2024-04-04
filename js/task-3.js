adb8d8b2e8cfa8154709d5070d6c68478b22accc
LisaUA <alontshka.777@gmail.com>
2024-04-04 19:04:27 +0300

    #1

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputRef = document.querySelector("#name-input");
-const outputRef = document.querySelector("#name-output");
-
-inputRef.addEventListener("input", (event) => {
-    outputRef.textContent = event.currentTarget.value.trim()
-    ? event.currentTarget.value 
-    : `Anonymous`;
-});
